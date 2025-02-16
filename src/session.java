9d8b25b46a16f9b27f34324d499a06ab4671d5f2
David <113213602+25dmvv@users.noreply.github.com>
2025-02-16 03:09:05 +0000

    moved to eclipse

--- src/session.java
+++ src/session.java
@@ -9,23 +9,22 @@ public class Session {
     private String startTime;
     private int duration;
     private int availableSpaces;
-}
 
     // Constructor Method
-    public Session(String sessionName, String levelofFitness, String day, String startTime, int duration, int availableSpaces) {
-        this.sessionID = sessionCounter++;
+    public Session(String sessionName, String levelOfFitness, String day, String startTime, int duration,
+            int availableSpaces) {
+        this.sessionID = sessionCounter;
+        sessionCounter++;
         this.sessionName = sessionName;
         this.levelOfFitness = levelOfFitness;
         this.day = day;
         this.startTime = startTime;
         this.duration = duration;
-        this.availableSpaces = availableSpaces; 
+        this.availableSpaces = availableSpaces;
 
-
-    
     }
 
-    //  Getters
+    // Getters
     public int getSessionID() {
         return sessionID;
     }
@@ -58,19 +57,25 @@ public class Session {
     public boolean register() {
         if (availableSpaces > 0) {
             availableSpaces--;
-            return true; 
+            return true;
         } else {
             return false; // if there are no spaces left
         }
-     }
+    }
 
     // Cancelling a registration
     public boolean cancel() {
         availableSpaces++;
-        return true;    
-     }
+        return true;
+    }
 
     // Prints session details
     public void printDetails() {
-        System.out.println("Session ID: " + sessionID + " || " + sessionName + " || " + levelOfFitness + " || " + day + " || " + startTime +  " || Duration " + duration + " mins || Spaces Left: " + availableSpaces);
-     }
+        System.out.println("Session ID: " + sessionID + " || " + sessionName + " || " + levelOfFitness + " || " + day
+                + " || " + startTime + " || Duration " + duration + " mins || Spaces Left: " + availableSpaces);
+    }
+
+}
+
+
+
